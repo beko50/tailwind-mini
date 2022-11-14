879685b614bd7edd94d2e8afd21e74d2dc071b13
Beko <essiamah20@gmail.com>
2022-11-14 18:09:36 +0000

    More to be done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: { 
+      sm: '480px',
+      md: '768px', 
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightOrange: '#ea580c',
+        lightOrange: '#d98c3f',
+        deepBlue: '#0369a1',
+        seaBlue: '#38bdf8',
+        teal: '#0d9488',
+        greyBlue: '#1f2937',
+      },
+    },
+  },
+  plugins: [],
+}
